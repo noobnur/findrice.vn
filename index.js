53201f5d6623fcc56a25ac7df9e33883abb653a0
Nur Hidayah Sanwari <nurhsans@gmail.com>
2018-03-25 10:19:31 +0800

    set up routes and views working

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 
 
-// const dbUrl
-const port = 3000
+const dbUrl = 'mongodb://localhost/Sentosa'
+const port = 4000
 
-const app = require('express')
+const app = express()
 
-// mongoose.connect(dbUrl, {
-//   useMongoClient: true
-// })
-// .then(
-//   () => { console.log('db is connected') },
-//   (err) => { console.log(err) }
-// )
+mongoose.connect(dbUrl, {
+  useMongoClient: true
+})
+.then(
+  () => { console.log('db is connected') },
+  (err) => { console.log(err) }
+)
 
 // ======= Use Middlewares ====== //
 app.use(express.static(path.join(__dirname, 'public')))
@@ -32,6 +33,21 @@ app.use(function (req, res, next) {
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+app.get('/', (req, res) => {
+  res.render('homepage')
+})
+
 app.get('/about', (req, res) => {
   res.render('about')
+})
+
+app.get('/prospect', (req, res) => {
+  res.render('prospect')
+})
+
+
+
+// ======   ====== //
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
